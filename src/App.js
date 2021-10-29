f5f13fc4674acf1be1fd5544eb65bcdbc3735934
LeeCH-OeO <fairprice81000@gmail.com>
2021-10-30 02:51:39 +0800

    update footer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
       <Typography className="header" variant="h3" gutterBottom style={{'font-family': 'Noto Serif TC','color':'red'}}>
         😀寶可夢圖鑑👍🏿
         </Typography>
+        <footer flex>
+          <a href="https://github.com/LeeCH-OeO/pokemon" target="_blank" rel="noreferrer">
+              <Typography align="center" color="textSecondary" gutterBottom >
+          ©LeeCH-OeO 
+          </Typography></a>         
+          </footer>
       <canvas id="myCanvas" width="630" height="630"></canvas> 
       <canvas id="myCanvas2" width="630" height="630"></canvas>
       <canvas id="myCanvas3" width="630" height="630"></canvas>
